4a8641d6acacf3725f0ce69b15f6531066abe0ab
Ari Mannila (IDS) <ari.mannila@flir.se>
2014-01-28 15:06:49 +0100

    Added control of video framerate and timestamp overlay into 'public' property tree.
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 186638]

--- bifrost/valhalla_ocg.h
+++ bifrost/valhalla_ocg.h
@@ -18,14 +18,14 @@ namespace VALHALLA_OCG
     const uint32_t      BASE_ADDRESS              =  0x00003000;
 
     /* CONTROL1 register */
-    const uint32_t      CONTROL1_OFFSET             = 0x0F20;      ///< offset relative to unit base address
+    const uint32_t      CONTROL1_OFFSET             = 0x0010;      ///< offset relative to unit base address
     const uint32_t      VIDEO_ROWS_MASK             = (0xFFFF << 16);
     const uint_fast8_t  VIDEO_ROWS_SHIFT            = 16;
     const uint32_t      VIDEO_COLUMNS_MASK          = (0xFFFF << 0);
     const uint_fast8_t  VIDEO_COLUMNS_SHIFT         = 0;
 
     /* CONTROL2 register */
-    const uint32_t      CONTROL2_OFFSET             = 0x0F24;      ///< offset relative to unit base address
+    const uint32_t      CONTROL2_OFFSET             = 0x0014;      ///< offset relative to unit base address
     const uint32_t      NUM_DISPLAYED_CHARS_MASK    = (0xFF << 16);
     const uint_fast8_t  NUM_DISPLAYED_CHARS_SHIFT   = 16;
     const uint32_t      CHAR_HEIGHT_MASK            = (0xFF << 8);
@@ -34,14 +34,14 @@ namespace VALHALLA_OCG
     const uint_fast8_t  CHAR_WIDTH_SHIFT            = 0;
 
     /* CONTROL3 register */
-    const uint32_t      CONTROL3_OFFSET             = 0x0F28;      ///< offset relative to unit base address
+    const uint32_t      CONTROL3_OFFSET             = 0x0018;      ///< offset relative to unit base address
     const uint32_t      CHAR_ROW_POSITION_MASK      = (0xFFFF << 16);
     const uint_fast8_t  CHAR_ROW_POSITION_SHIFT     = 16;
     const uint32_t      CHAR_COLUMN_POSITION_MASK   = (0xFFFF << 0);
     const uint_fast8_t  CHAR_COLUMN_POSITION_SHIFT  = 0;
 
     /* CONTROL4 register */
-    const uint32_t      CONTROL4_OFFSET             = 0x0F2C;      ///< offset relative to unit base address
+    const uint32_t      CONTROL4_OFFSET             = 0x001C;      ///< offset relative to unit base address
     const uint32_t      FG_COLOR_A_MASK             = (0xFF << 24);
     const uint_fast8_t  FG_COLOR_A_SHIFT            = 24;
     const uint32_t      FG_COLOR_R_MASK             = (0xFF << 16);
@@ -52,7 +52,7 @@ namespace VALHALLA_OCG
     const uint_fast8_t  FG_COLOR_B_SHIFT            = 0;
 
     /* CONTROL5 register */
-    const uint32_t      CONTROL5_OFFSET             = 0x0F30;      ///< offset relative to unit base address
+    const uint32_t      CONTROL5_OFFSET             = 0x0020;      ///< offset relative to unit base address
     const uint32_t      BG_COLOR_A_MASK             = (0xFF << 24);
     const uint_fast8_t  BG_COLOR_A_SHIFT            = 24;
     const uint32_t      BG_COLOR_R_MASK             = (0xFF << 16);
@@ -63,14 +63,14 @@ namespace VALHALLA_OCG
     const uint_fast8_t  BG_COLOR_B_SHIFT            = 0;
 
     /* Character map, defines the character to display */
-    const uint32_t      CHAR_MAP_BASE               = 0x0000;
+    const uint32_t      CHAR_MAP_BASE               = 0x0100;
     const uint32_t      EXTERNAL_TIME_REF_MASK      = (0xF << 12);
     const uint_fast8_t  EXTERNAL_TIME_REF_SHIFT     = 12;
     const uint32_t      CHAR_ADDRESS_MASK           = (0xFFF << 0);
     const uint_fast8_t  CHAR_ADDRESS_SHIFT          = 0;
 
     /* Pixel map, defines the character appearance */
-    const uint32_t      PIXEL_MAP_BASE              = 0x0400;
+    const uint32_t      PIXEL_MAP_BASE              = 0x0500;
 }
 
 #endif
--- bifrost/valhalla_vck.h
+++ bifrost/valhalla_vck.h
@@ -15,20 +15,37 @@
 
 namespace VALHALLA_VCK
 {
-    const uint32_t      BASE_ADDRESS          =  0x00000B00;
+    const uint32_t      BASE_ADDRESS_HDC        = 0x00000B00;
+    const uint32_t      BASE_ADDRESS_HDC_MS     = 0x00000300;
 
     /* I2C_RST */
-    const uint32_t      I2C_RST_OFFSET        =  0x0010;      ///< offset relative to unit base address
-    const uint32_t      I2C_RESET_MASK        = (1 << 0);     ///< bit[0]= I2C Reset, active low
+    const uint32_t      I2C_RST_OFFSET          = 0x0010;       ///< offset relative to unit base address
+    const uint32_t      I2C_RESET_MASK          = (1 << 0);     ///< bit[0]= I2C Reset, active low
+
+    /* I2C_RUN_SEQ */
+    const uint32_t      I2C_RUN_SEQ_OFFSET      = 0x0014;       ///< run i2c command sequence
 
     /* I2C_DEVICEADDR */
-    const uint32_t      I2C_DEVICEADDR_OFFSET =  0x0018;      ///< offset relative to unit base address
-    const uint32_t      I2C_DEVICEADDR_MASK   = (0xFF << 0);  ///< bit[7:0]= I2C Device address
-    const uint_fast8_t  I2C_DEVICEADDR_SHIFT  =  0;
+    const uint32_t      I2C_DEVICEADDR_OFFSET   = 0x0018;       ///< offset relative to unit base address
+    const uint32_t      I2C_DEVICEADDR_MASK     = (0xFF << 0);  ///< bit[7:0]= I2C Device address
+    const uint_fast8_t  I2C_DEVICEADDR_SHIFT    = 0;
+
+    /* I2C_REGISTERADDR */
+    const uint32_t      I2C_REGISTERADDR_OFFSET = 0x001C;       ///< offset relative to unit base address
+    const uint32_t      I2C_REGISTERADDR_MASK   = (0xFF << 0);  ///< bit[7:0]= I2C Register address (i.e command)
+    const uint_fast8_t  I2C_REGISTERADDR_SHIFT  = 0;
 
     /* I2C_WRITE_REG */
-    const uint32_t      I2C_WRITE_REG_OFFSET  =  0x0020;      ///< offset relative to unit base address
-    // bit[31:0]= I2C write data
+    const uint32_t      I2C_WRITE_REG_OFFSET    = 0x0020;       ///< offset relative to unit base address
+    // bit[31:0] = I2C write data
+
+    /* I2C_READ_REG */
+    const uint32_t      I2C_READ_REG_OFFSET     = 0x0024;       ///< offset relative to unit base address
+    // bit[0] = I2C READ ACC, Hmmm actually not sure what to do with this register... Somekind of status?
+
+    /* I2C_READ_STATUS */
+    const uint32_t      I2C_READ_STATUS_OFFSET  = 0x0080;       ///< offset relative to unit base address
+    // bit[31:0] = I2C read data (or shall it be, status data? I don't know the FPGA documentation says data...
 }
 
 #endif
