a0f5ff8c59a04f0f4f0cb430d4a96568a0790204
Ari Mannila (IDS) <ari.mannila@flir.se>
2014-01-30 13:48:30 +0100

    Added skeleton for controlling the ARNF-block (Adaptive Row Noise Filter).
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 186761]

---
+++ bifrost/valhalla_arnf.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) FLIR Systems AB. All rights reserved.
+ *
+ * valhalla_arnf.h
+ *
+ *  Created on: Jan 21, 2014
+ *      Author: Ari Mannila <ari.mannila@flir.se>
+ *
+ * Register map for FPGA Valhalla ARNF (Adaptive Row Noise Filter) block.
+ *
+ */
+
+#ifndef VALHALLA_ARNF_H_
+#define VALHALLA_ARNF_H_
+
+namespace VALHALLA_ARNF
+{
+//    const uint32_t      BASE_ADDRESS              =  0x0000????;
+
+
+/*
+    const uint32_t      CONTROL1_OFFSET             = 0x0020;      ///< offset relative to unit base address
+    const uint32_t      VIDEO_ROWS_MASK             = (0xFFFF << 16);
+    const uint_fast8_t  VIDEO_ROWS_SHIFT            = 16;
+    const uint32_t      VIDEO_COLUMNS_MASK          = (0xFFFF << 0);
+    const uint_fast8_t  VIDEO_COLUMNS_SHIFT         = 0;
+*/
+}
+
+#endif
