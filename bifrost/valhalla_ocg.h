1c943dca84123edc56b0c1076bee0f4b6c82981c
Ari Mannila (IDS) <ari.mannila@flir.se>
2014-01-30 12:18:31 +0100

    Adjusted some timeouts to let SDI HW to settle when changing video framerate. Changed addresses for the OCG registerblock as documentation for the FPGA was incorrect.
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 186752]

--- bifrost/valhalla_ocg.h
+++ bifrost/valhalla_ocg.h
@@ -18,14 +18,14 @@ namespace VALHALLA_OCG
     const uint32_t      BASE_ADDRESS              =  0x00003000;
 
     /* CONTROL1 register */
-    const uint32_t      CONTROL1_OFFSET             = 0x0010;      ///< offset relative to unit base address
+    const uint32_t      CONTROL1_OFFSET             = 0x0020;      ///< offset relative to unit base address
     const uint32_t      VIDEO_ROWS_MASK             = (0xFFFF << 16);
     const uint_fast8_t  VIDEO_ROWS_SHIFT            = 16;
     const uint32_t      VIDEO_COLUMNS_MASK          = (0xFFFF << 0);
     const uint_fast8_t  VIDEO_COLUMNS_SHIFT         = 0;
 
     /* CONTROL2 register */
-    const uint32_t      CONTROL2_OFFSET             = 0x0014;      ///< offset relative to unit base address
+    const uint32_t      CONTROL2_OFFSET             = 0x0024;      ///< offset relative to unit base address
     const uint32_t      NUM_DISPLAYED_CHARS_MASK    = (0xFF << 16);
     const uint_fast8_t  NUM_DISPLAYED_CHARS_SHIFT   = 16;
     const uint32_t      CHAR_HEIGHT_MASK            = (0xFF << 8);
@@ -34,14 +34,14 @@ namespace VALHALLA_OCG
     const uint_fast8_t  CHAR_WIDTH_SHIFT            = 0;
 
     /* CONTROL3 register */
-    const uint32_t      CONTROL3_OFFSET             = 0x0018;      ///< offset relative to unit base address
+    const uint32_t      CONTROL3_OFFSET             = 0x0028;      ///< offset relative to unit base address
     const uint32_t      CHAR_ROW_POSITION_MASK      = (0xFFFF << 16);
     const uint_fast8_t  CHAR_ROW_POSITION_SHIFT     = 16;
     const uint32_t      CHAR_COLUMN_POSITION_MASK   = (0xFFFF << 0);
     const uint_fast8_t  CHAR_COLUMN_POSITION_SHIFT  = 0;
 
     /* CONTROL4 register */
-    const uint32_t      CONTROL4_OFFSET             = 0x001C;      ///< offset relative to unit base address
+    const uint32_t      CONTROL4_OFFSET             = 0x002C;      ///< offset relative to unit base address
     const uint32_t      FG_COLOR_A_MASK             = (0xFF << 24);
     const uint_fast8_t  FG_COLOR_A_SHIFT            = 24;
     const uint32_t      FG_COLOR_R_MASK             = (0xFF << 16);
@@ -52,7 +52,7 @@ namespace VALHALLA_OCG
     const uint_fast8_t  FG_COLOR_B_SHIFT            = 0;
 
     /* CONTROL5 register */
-    const uint32_t      CONTROL5_OFFSET             = 0x0020;      ///< offset relative to unit base address
+    const uint32_t      CONTROL5_OFFSET             = 0x0030;      ///< offset relative to unit base address
     const uint32_t      BG_COLOR_A_MASK             = (0xFF << 24);
     const uint_fast8_t  BG_COLOR_A_SHIFT            = 24;
     const uint32_t      BG_COLOR_R_MASK             = (0xFF << 16);
