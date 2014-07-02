70bec4f889f266af89598f9b22756727013a555a
Ari Mannila (IDS) <ari.mannila@flir.se>
2014-07-02 10:42:41 +0100

    Added pixel value clip control property into GOC.
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 194203]

--- bifrost/valhalla_goc.h
+++ bifrost/valhalla_goc.h
@@ -27,6 +27,7 @@
 
 #define VALHALLA_GOC_CONTROL1_GAIN_ENABLE     (1 << 0) // enable gain map
 #define VALHALLA_GOC_CONTROL1_DEAD_SELECT     (1 << 1) // if set, set value of pixel if gain data is 0
+#define VALHALLA_GOC_CONTROL1_CLIP_ENABLE     (1 << 3) // enable clip to prevent pixels to wrap-around
 
 #define VALHALLA_GOC_CONTROL2_OFFSET_ENABLE   (1 << 1) // enable offset map
 #define VALHALLA_GOC_CONTROL2_COARSE_ENABLE   (1 << 2) // enable coarse map
