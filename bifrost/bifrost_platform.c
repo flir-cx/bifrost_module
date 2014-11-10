25ffed405260f0a563a145e7504010b5c485ebc4
tokarlss <a@b>
2014-11-10 07:51:23 +0100

    bifrost_platform: missing bool declaration (v2.6.37), added linux/types.h
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 199167]

--- bifrost/bifrost_platform.c
+++ bifrost/bifrost_platform.c
@@ -1,15 +1,19 @@
-
+/*
+ * Copyright (c) FLIR Systems AB.
+ */
 #include <linux/version.h>
+#include "bifrost_platform.h"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,10,0)
 #include "../arch/arm/mach-imx/hardware.h"
 #else
-bool cpu_is_imx6q(){return false;}
+bool cpu_is_imx6q(void)
+{
+	return false;
+}
 #endif
 
 bool platform_rocky(void)
 {
-
-    return cpu_is_imx6q();
-
+	return cpu_is_imx6q();
 }
--- bifrost/bifrost_platform.h
+++ bifrost/bifrost_platform.h
@@ -1,11 +1,11 @@
-
-
-
+/*
+ * Copyright (c) FLIR Systems AB.
+ */
 #ifndef BIFROST_PLATFORM_H_
 #define BIFROST_PLATFORM_H_
 
+#include <linux/types.h>
 
-bool platform_rocky(void);
-
+extern bool platform_rocky(void);
 
 #endif
