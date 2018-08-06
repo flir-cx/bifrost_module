f5c3ef1dfd84e8ff88c4a7d073701b77f2fde843
Klas Malmborg (ES) <klas.malmborg@flir.se>
2018-08-06 13:51:21 +0100

    Free IRQ2 in bifrost_fvd_exit
    [git-p4: depot-paths = "//depot/GSS/Heimdall/maximal/modules/": change = 255237]

--- bifrost/bifrost_membus.c
+++ bifrost/bifrost_membus.c
@@ -259,6 +259,8 @@ void bifrost_fvd_exit(struct bifrost_device *dev)
         gpio_free(FPGA_IRQ_0);
         free_irq(gpio_to_irq(FPGA_IRQ_1), bdev);
         gpio_free(FPGA_IRQ_1);
+        free_irq(gpio_to_irq(FPGA_IRQ_2), bdev);
+        gpio_free(FPGA_IRQ_2);
         device_destroy(dev->pClass, dev->cdev.dev);
         class_destroy(dev->pClass);
         platform_device_unregister(dev->pMemDev);
