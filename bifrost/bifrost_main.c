d74042d04ff6efaf9f354a44e3cacff6d85b43de
bo svangård <bo.svangard@flir.se>
2024-04-17 11:29:09 +0200

    drop procfs file

--- bifrost/bifrost_main.c
+++ bifrost/bifrost_main.c
@@ -22,13 +22,8 @@
 #include <linux/time.h>
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0)
 #include <linux/uaccess.h>
 #include <linux/atomic.h>
-#else
-#include <linux/uaccess.h>
-#include <linux/atomic.h>
-#endif
 
 #include <asm/byteorder.h>
 
@@ -49,108 +44,6 @@ MODULE_PARM_DESC(simulator, "Enable simulator interface");
 static u32 membus;
 module_param(membus, uint, 0400);
 MODULE_PARM_DESC(membus, "Enable memory bus interface to FPGA (instead of PCI)");
-#if KERNEL_VERSION(3, 10, 0) > LINUX_VERSION_CODE
-static int device_mem_info(struct bifrost_device *dev, char *buf, size_t bufsz)
-{
-	int bar, len, n;
-	struct device_memory *mem;
-
-	for (len = 0, bar = 0; bar < ARRAY_SIZE(dev->regb); bar++) {
-		mem = &dev->regb[bar];
-		if (mem->enabled)
-			n = snprintf(buf + len,
-				     bufsz - len,
-				     "BAR%d: bus-addr=%08lx cpu-addr=%p size=%lu KByte\n",
-				     bar,
-				     mem->addr_bus,
-				     mem->addr,
-				     mem->size / 1024);
-		else
-			n = snprintf(buf + len,
-				     bufsz - len,
-				     "BAR%d: disabled\n",
-				     bar);
-		if (n < 0)
-			return len;
-		len += n;
-	}
-	return len;
-}
-
-/**
- * Put data into the proc fs file.
- *
- * @param page Where to start write data
- * @param start Where in page
- * @param offset in page that read is done
- * @param page_size Size of page (bytes)
- * @param eof Set to 1 to signal EOF
- * @return 0 to indicate that there is no more data to read.
- */
-static int bifrost_procfs_read(char *page, char **start, off_t offset,
-			       int page_size, int *eof, void *data)
-{
-	struct bifrost_device *dev = (struct bifrost_device *)data;
-	int len;
-
-	INFO("read /proc/%s\n", BIFROST_DEVICE_NAME);
-	dev->stats.procfsreads++;
-
-	/*
-	 * Driver gives all information in one go, hence if user requests
-	 * more information return EOF.
-	 */
-	if (offset > 0) {
-		*eof = 1;
-		return 0;
-	}
-
-	len = snprintf(page, page_size,
-		       "Device name		 : %s\n"
-		       "Driver version		 : %d.%d.%d%s\n"
-		       "Mode			 : %s\n"
-		       "Interface		 : %s\n",
-		       BIFROST_DEVICE_NAME,
-		       BIFROST_VERSION_MAJOR,
-		       BIFROST_VERSION_MINOR,
-		       BIFROST_VERSION_MICRO,
-		       BIFROST_VERSION_APPEND_STR,
-		       dev->info.simulator == 1 ? "simulator" : "normal",
-		       dev->membus == 1 ? "memory bus" : "PCIe");
-
-	len += device_mem_info(dev, &page[len], page_size - len);
-
-	len += snprintf(&page[len], page_size - len,
-			"Open ops.		  : %ld\n"
-			"Release ops.		  : %ld\n"
-			"Poll ops.		  : %ld\n"
-			"Read buffer size	  : %d\n"
-			"Read buffer head	  : %d\n"
-			"Read buffer tail	  : %d\n"
-			"Read buffer overruns	  : %d\n"
-			"Read ops.		  : %ld\n"
-			"Read bytes		  : %ld\n"
-			"Write ops.		  : %ld\n"
-			"Written bytes		  : %ld\n"
-			"Ioctl ops.		  : %ld\n"
-			"Lseek ops.		  : %ld\n"
-			"Proc fs reads		  : %ld\n",
-			dev->stats.opens,
-			dev->stats.releases,
-			dev->stats.polls,
-			CIRCULAR_BUFFER_SIZE,
-			0, 0, 0,
-			dev->stats.reads,
-			dev->stats.read_b,
-			dev->stats.writes,
-			dev->stats.write_b,
-			dev->stats.ioctls,
-			dev->stats.llseeks,
-			dev->stats.procfsreads);
-	*eof = 1;
-	return len;
-}
-#endif
 
 void bifrost_dma_chan_start(void *data, u32 ch, u32 src, u32 dst, u32 len,
 			    u32 dir)
@@ -308,20 +201,6 @@ static int __init bifrost_init(void)
 		goto err_alloc_queue;
 	}
 
-#if KERNEL_VERSION(3, 10, 0) > LINUX_VERSION_CODE
-	/*
-	 * Setup /proc file system entry. Create a read-only entry in
-	 * proc root with a file name same as the device (use NULL as
-	 * parent dir)
-	 */
-	bdev->proc = create_proc_read_entry(BIFROST_DEVICE_NAME, 0, NULL,
-					    bifrost_procfs_read, bdev);
-	if (bdev->proc == NULL) {
-		ALERT("failed to add proc fs entry\n");
-		goto err_alloc_queue;
-	}
-#endif
-
 	if (bdev->info.simulator) {
 		if (bifrost_sim_pci_init(bdev) < 0)
 			goto err_pci;
@@ -355,9 +234,6 @@ static int __init bifrost_init(void)
 	return 0;
 
 err_pci:
-#if KERNEL_VERSION(3, 10, 0) > LINUX_VERSION_CODE
-	remove_proc_entry(BIFROST_DEVICE_NAME, NULL);
-#endif
 	flush_workqueue(work_queue);
 	destroy_workqueue(work_queue);
 err_alloc_queue:
@@ -375,9 +251,6 @@ err_alloc_pool:
 static void __exit bifrost_exit(void)
 {
 	INFO("exit\n");
-#if KERNEL_VERSION(3, 10, 0) > LINUX_VERSION_CODE
-	remove_proc_entry(BIFROST_DEVICE_NAME, NULL);
-#endif
 	bifrost_cdev_exit(bdev);
 	bifrost_dma_cleanup(bdev);
 
